c19edf8a370b343fe95a32464dc93caadea570c2
sheikhaGVSU <sheikha@mail.gvsu.edu>
2019-02-08 12:53:36 -0500

    removed unneeded functions from sort_cards.js

--- prototype/static/js/sort_cards.js
+++ prototype/static/js/sort_cards.js
@@ -56,10 +56,14 @@ function simulateSealed (commons, uncommons, rares, mythics) {
     //numMythics compared against "magic number" 6 because it's the max possible mythics
     if(commons.length >= numCommons && uncommons.length >= numUncommons &&
         rares.length >= numRares && mythics.length >= 6 ){
+        
+        //reset sealedresult in case an existing sealed is stored
+        sealedresult.clear();
+        
         //determine how many rares will become mythics for the draft
-        for(var i=0; i < 6; i++){
+        for(let i=0; i < 6; i++){
             //random number from 0 to 999
-            var mythic = Math.floor(Math.random() * 1000);
+            let mythic = Math.floor(Math.random() * 1000);
             //"magic number" 124 derived from 12.5% chance for a rare to become a mythic
             if (mythic <= 124){
                 numMythics++;
@@ -69,29 +73,29 @@ function simulateSealed (commons, uncommons, rares, mythics) {
         numRares -= numMythics;
         
         //select 60 random commons
-        for(var i = 0; i < numCommons; i++){
+        for(let i = 0; i < numCommons; i++){
             //select an index
-            var tempIndex = Math.floor(Math.random() * commons.length);
+            let tempIndex = Math.floor(Math.random() * commons.length);
             //add card to sealedresult
             sealedresult.push(commons[tempIndex]);
             //remove card from rarity array to avoid double-drafting it
             commons.splice(tempIndex,1);
         }
         //select 18 random uncommons
-        for(var i = 0; i < numUncommons; i++){
-            var tempIndex = Math.floor(Math.random() * uncommons.length);
+        for(let i = 0; i < numUncommons; i++){
+            let tempIndex = Math.floor(Math.random() * uncommons.length);
             sealedresult.push(uncommons[tempIndex]);
             uncommons.splice(tempIndex,1);
         }
         //select up to 6 random rares
-        for(var i = 0; i < numRares; i++){
-            var tempIndex = Math.floor(Math.random() * rares.length);
+        for(let i = 0; i < numRares; i++){
+            let tempIndex = Math.floor(Math.random() * rares.length);
             sealedresult.push(rares[tempIndex]);
             rares.splice(tempIndex,1);
         }
         //select up to 6 random mythics
-        for(var i = 0; i < numMythics; i++){
-            var tempIndex = Math.floor(Math.random() * mythics.length);
+        for(let i = 0; i < numMythics; i++){
+            let tempIndex = Math.floor(Math.random() * mythics.length);
             sealedresult.push(mythics[tempIndex]);
             mythics.splice(tempIndex,1);
         }
@@ -100,46 +104,7 @@ function simulateSealed (commons, uncommons, rares, mythics) {
     }
 }
 
-$('.add-card').on('click', function(event) {
-    $.ajax({
-        data : {
-            add_card : $(this).val()
-        },
-        type : 'POST',
-        url: '/add'
-    })
-    .done(function(data) {
-        if(data.error) {
-            console.log(data.error);
-        }
-        else {
-            display_cards(data.success);
-        }
-    })
-    event.preventDefault();
-});
-
-$('#clear-deck').on('click', function(event) {
-    $.ajax({
-        data : {
-            clear : "clear"
-        },
-        type : 'POST',
-        url: '/clear'
-    })
-    .done(function(data) {
-        if(data.error) {
-            console.log(data.error);
-        }
-        else {
-            collection = JSON.parse(data.success);
-            $("#sealed-deck").text("");
-            let total = deck['count'];
-            $(".total-cards").text(`Total cards: ${total}`);
-        }
-    });
-    event.preventDefault();
-});
+btnSimSealed.onclick = function(){simulateSealed(commons, uncommons, rares, mythics);};
 
 function display_cards(data) {
     deck = JSON.parse(data);
