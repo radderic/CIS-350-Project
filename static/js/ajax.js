e6b66d82d099b14dd17488b1bf6fb9ad4d261336
sheikhaGVSU <sheikha@mail.gvsu.edu>
2019-02-17 21:53:44 -0500

    Fixed style inconsistencies in the JS files, removed a legacy variable from sort_cards, changed function names to consistently use underscore style rather than camelcase. Removed an unused webpage. Added a .jshintrc file to configure the js bugfinder.

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,98 +1,99 @@
+"use strict";
 var collection;
 
 $.ajax({
     data : {
         fetch : "fetch"
     },
-    type : 'POST',
-    url: '/fetch'
+    type : "POST",
+    url: "/fetch"
 })
 .done(function(data) {
     if(data.error) {
-        console.log(data.error)
+        console.log(data.error);
     }
     else {
         display_cards(data.success);
     }
-})
+});
 
-$('.add-card').on('click', function(event) {
+$(".add-card").on("click", function(event) {
     $.ajax({
         data : {
             add_card : $(this).val()
         },
-        type : 'POST',
-        url: '/add'
+        type : "POST",
+        url: "/add"
     })
     .done(function(data) {
         if(data.error) {
-            console.log(data.error)
+            console.log(data.error);
         }
         else {
             display_cards(data.success);
         }
-    })
+    });
     event.preventDefault();
 });
 
-$('#clear-deck').on('click', function(event) {
+$("#clear-deck").on("click", function(event) {
     $.ajax({
         data : {
             clear : "clear"
         },
-        type : 'POST',
-        url: '/clear'
+        type : "POST",
+        url: "/clear"
     })
     .done(function(data) {
         if(data.error) {
-            console.log(data.error)
+            console.log(data.error);
         }
         else {
             collection = JSON.parse(data.success);
             $("#draft-deck").text("");
-            let total = collection['count']
+            let total = collection.count;
             $(".total-cards").text(`Total cards: ${total}`);
         }
-    })
+    });
     event.preventDefault();
 });
 
 
 function reload_buttons() {
-    $('.add-card').off().click(function(event) {
+    $(".add-card").off().click(function(event) {
         $.ajax({
             data : {
                 add_card : $(this).val()
             },
-            type : 'POST',
-            url: '/add'
+            type : "POST",
+            url: "/add"
         })
         .done(function(data) {
             if(data.error) {
-                console.log(data.error)
+                console.log(data.error);
             }
             else {
                 display_cards(data.success);
             }
-        })
+        });
         event.preventDefault();
     });
-    $('.sub-card').off().click(function(event) {
+    $(".sub-card").off().click(function(event) {
         $.ajax({
             data : {
                 sub_card : $(this).val()
             },
-            type : 'POST',
-            url: '/sub'
+            type : "POST",
+            url: "/sub"
         })
         .done(function(data) {
             if(data.error) {
-                console.log(data.error)
+                console.log(data.error);
             }
             else {
                 display_cards(data.success);
             }
-        })
+        });
         event.preventDefault();
     });
 
@@ -100,13 +101,13 @@ function reload_buttons() {
 
 function display_cards(data) {
     collection = JSON.parse(data);
-    let total = collection['count']
+    let total = collection.count;
     $("#draft-deck").text("");
     $(".total-cards").text(`Total cards: ${total}`);
-    for(card in collection) {
-        if(card !== 'count') {
-            let name = collection[card]['card_name']
-            let count = collection[card]['count']
+    for(var card in collection) {
+        if(card !== "count") {
+            let name = collection[card].card_name;
+            let count = collection[card].count;
             $("#draft-deck").append(`<div class="collection-card">
                 <p>${name} x ${count}</p>
                 <button value=${card} name="add_card" class="add-card">+</button>
--- static/js/sort_cards.js
+++ static/js/sort_cards.js
@@ -1,3 +1,4 @@
+"use strict";
 //all cards
 var collection;
 var sealedresult = [];
@@ -12,8 +13,8 @@ $.ajax({
     data : {
         fetch : "fetch"
     },
-    type : 'POST',
-    url: '/fetch'
+    type : "POST",
+    url: "/fetch"
 })
 .done(function(data) {
     if(data.error) {
@@ -21,23 +22,23 @@ $.ajax({
     }
     else {
         collection = JSON.parse(data.success);
-        sortRarity(collection);
-        addButtons();
+        sort_rarity(collection);
+        add_buttons();
     }
 });
 
-function sortRarity(collection) {
+function sort_rarity(collection) {
     for(var card in collection) {
-        if(card !== 'count') {
+        if(card !== "count") {
             let count = collection[card].count;
             for(var i = 0; i < count; i++) {
-                if(collection[card].rarity === 'Common'){
+                if(collection[card].rarity === "Common"){
                     commons.push(card);
-                }else if(collection[card].rarity === 'Uncommon'){
+                }else if(collection[card].rarity === "Uncommon"){
                     uncommons.push(card);
-                }else if(collection[card].rarity === 'Rare'){
+                }else if(collection[card].rarity === "Rare"){
                     rares.push(card);
-                }else if(collection[card].rarity === 'Mythic'){
+                }else if(collection[card].rarity === "Mythic"){
                     mythics.push(card);
                 }
                 //else invalid rarity - add warning here in second release?
@@ -46,14 +47,14 @@ function sortRarity(collection) {
     }
 }
 
-function addButtons(){
+function add_buttons(){
     $(".controls").append(`
-    <button onclick="sort_cards:simulateSealed()">Simulate Sealed</button>
-    <button  onclick="sort_cards:clearDeck()">Clear Deck</button>
+    <button onclick="sort_cards:simulate_sealed()">Simulate Sealed</button>
+    <button  onclick="sort_cards:clear_deck()">Clear Deck</button>
     `);
 }
 
-function simulateSealed () {
+function simulate_sealed () {
     //numbers of each rarity opened in a sealed draft - rares and mythics subject to change
     var numCommons = 60;
     var numUncommons = 18;
@@ -100,7 +101,7 @@ function simulateSealed () {
             }
             else if(tempRares.length > 0) {
                 let tempIndex = Math.floor(Math.random() * tempRares.length);
-                sealedresult.push(rares[tempIndex]);
+                sealedresult.push(tempRares[tempIndex]);
                 tempRares.splice(tempIndex,1);
             }
             // we must have run out of rares, use mythics
@@ -112,7 +113,7 @@ function simulateSealed () {
         }
         display_cards();
     }
-    //else colllection needs more cards TODO print warning to user
+    //else collection needs more cards TODO print warning to user
 }
 
 function display_cards() {
@@ -124,8 +125,8 @@ function display_cards() {
     //display each element in sealedresult
     for(var index in sealedresult) {
         let card_id = sealedresult[index];
-        let card_img = collection[card_id]['image_url'];
-        let name = collection[card_id]['card_name'];
+        let card_img = collection[card_id].image_url;
+        let name = collection[card_id].card_name;
         $("#sealed-results").append(`<div class="sealed-card" id="${card_id}">
                 <img onclick="sort_cards:add_to_deck(${card_id})" src=${card_img} alt=${name}/>
             </div>`);
@@ -134,8 +135,7 @@ function display_cards() {
     for(var key in deck){
         let card_id = key;
         let count = deck[key];
-        let card_img = collection[card_id]['image_url'];
-        let name = collection[card_id]['card_name'];
+        let name = collection[card_id].card_name;
         $("#sealed-picks").append(`<div class="deck-card" id="${card_id}">
                 <p onclick="sort_cards:remove_from_deck(${card_id})">${count}x ${name} </p>
             </div>`);
@@ -176,7 +176,7 @@ function remove_from_deck(card_id){
     display_cards();
 }
 
-function clearDeck(){
+function clear_deck(){
     for(var card_id in deck){
         let count = deck[card_id];
         for(let i = 0; i < count; i++){
